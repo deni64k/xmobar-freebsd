9d68f4b270ab76eacaeca5ed9cf8d1a3c6624f1b
Andrea Rossato <andrea.rossato@ing.unitn.it>
2007-07-13 13:36:13 +0400

    added Plugins.hs for exporting the needed plugin API
    darcs-hash:20070713093613-d6583-ff3b5c080757cfd123f7f02e00612ef9bea0ab44.gz

---
+++ Plugins.hs
@@ -0,0 +1,20 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Xmobar.Plugins
+-- Copyright   :  (c) Andrea Rossato
+-- License     :  BSD-style (see LICENSE)
+-- 
+-- Maintainer  :  Andrea Rossato <andrea.rossato@unibz.it>
+-- Stability   :  unstable
+-- Portability :  unportable
+--
+-- This module export the API for plugins.
+--
+-- Have a look ate Plugins/HelloWorld.hs
+--
+-----------------------------------------------------------------------------
+
+module Plugins ( Exec (..) 
+               ) where
+
+import Commands
