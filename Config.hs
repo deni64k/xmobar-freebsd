4427b07952654eecb04f4bfa2a98b8b03c750c87
Andrea Rossato <andrea.rossato@ing.unitn.it>
2007-09-15 20:36:33 +0400

    Added Date to default config
    darcs-hash:20070915163633-d6583-b61b6e05ae604b24015dacd61510f650c4c6dee1.gz

--- Config.hs
+++ Config.hs
@@ -22,42 +22,43 @@ module Config ( -- * Configuration
 import Commands
 import {-# SOURCE #-} Runnable
 import Plugins.Monitors
+import Plugins.Date
 
 -- $config
 -- Configuration data type and default configuration
 
 -- | The configuration data type
 data Config = 
-    Config { font           :: String   -- ^ Font
-           , bgColor        :: String   -- ^ Backgroud color
-           , fgColor        :: String   -- ^ Default font color
-           , xPos           :: Int      -- ^ x Window position (origin in the upper left corner) 
-           , yPos           :: Int      -- ^ y Window position 
-           , width          :: Int      -- ^ Window width
-           , height         :: Int      -- ^ Window height
-           , align          :: String   -- ^ text alignment
-           , refresh        :: Int      -- ^ Refresh rate in tenth of seconds
+    Config { font           :: String     -- ^ Font
+           , bgColor        :: String     -- ^ Backgroud color
+           , fgColor        :: String     -- ^ Default font color
+           , xPos           :: Int        -- ^ x Window position (origin in the upper left corner) 
+           , yPos           :: Int        -- ^ y Window position 
+           , width          :: Int        -- ^ Window width
+           , height         :: Int        -- ^ Window height
+           , align          :: String     -- ^ text alignment
+           , refresh        :: Int        -- ^ Refresh rate in tenth of seconds
            , commands       :: [Runnable] -- ^ For setting the command, the command argujments 
-                                               -- and refresh rate for the programs to run (optional)
-           , sepChar        :: String -- ^ The character to be used for indicating
-                                      -- commands in the output template (default '%')
-           , template       :: String -- ^ The output template 
+                                          --   and refresh rate for the programs to run (optional)
+           , sepChar        :: String     -- ^ The character to be used for indicating
+                                          --   commands in the output template (default '%')
+           , template       :: String     -- ^ The output template 
            } deriving (Read)
 
 -- | The default configuration values
 defaultConfig :: Config
 defaultConfig =
-    Config { font = "-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*"
-           , bgColor = "#000000"
-           , fgColor = "#BFBFBF"
-           , xPos = 0
-           , yPos = 0
-           , width = 1024
-           , height = 15
-           , align = "left"
-           , refresh = 10
+    Config { font     = "-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*"
+           , bgColor  = "#000000"
+           , fgColor  = "#BFBFBF"
+           , xPos     = 0
+           , yPos     = 0
+           , width    = 1024
+           , height   = 15
+           , align    = "left"
+           , refresh  = 10
            , commands = []
-           , sepChar = "%"
+           , sepChar  = "%"
            , template = "Uptime: <fc=#00FF00>%uptime%</fc> ** <fc=#FF0000>%date%</fc>"
            }
 
@@ -67,5 +68,5 @@ defaultConfig =
 -- the 'Runnable.Runnable' Read instance. To install a plugin just add
 -- the plugin's type to the list of types appearing in this function's type
 -- signature.
-runnableTypes :: (Command,(Monitors,()))
+runnableTypes :: (Command,(Monitors,(Date,())))
 runnableTypes = undefined
