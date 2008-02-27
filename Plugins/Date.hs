f99d4fcda8fc0f769053eebdf0975b631cd56ffd
Andrea Rossato <andrea.rossato@ing.unitn.it>
2008-02-27 22:00:51 +0300

    Date: haddock fixes
    darcs-hash:20080227190051-d6583-1ed62c182e20c3a615a9af2706371ebe69862a0a.gz

--- Plugins/Date.hs
+++ Plugins/Date.hs
@@ -3,15 +3,16 @@
 -- Module      :  Plugins.Date
 -- Copyright   :  (c) Andrea Rossato
 -- License     :  BSD-style (see LICENSE)
--- 
+--
 -- Maintainer  :  Andrea Rossato <andrea.rossato@unibz.it>
 -- Stability   :  unstable
 -- Portability :  unportable
 --
 -- A date plugin for Xmobar
 --
--- Usage example: in template put 
--- Run Date "%a %b %_d %Y <fc=#ee9a00> %H:%M:%S</fc>" "mydate" 10
+-- Usage example: in template put
+--
+-- > Run Date "%a %b %_d %Y <fc=#ee9a00> %H:%M:%S</fc>" "Mydate" 10
 --
 -----------------------------------------------------------------------------
 
@@ -31,6 +32,6 @@ instance Exec Date where
     rate  (Date _ _ r) = r
 
 date :: String -> IO String
-date format = do 
+date format = do
   t <- toCalendarTime =<< getClockTime
   return $ formatCalendarTime defaultTimeLocale format t
