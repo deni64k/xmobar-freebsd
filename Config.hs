6c7eaf1a051353e7e18fad4373a41a7141f71166
Spencer Janssen <sjanssen@cse.unl.edu>
2009-11-07 04:01:04 +0300

    Add XMonadLog.
    Ignore-this: 99ce96d00494d6bd681ef524bb93f8c7
    Support for writing WM status to a root window property was just added to
    xmonad, this is the accompanying xmobar plugin.  The idea is to use XMonadLog
    rather than StdinReader with xmonad, this should hopefully be more robust than
    communicating over a pipe.
    
    darcs-hash:20091107010104-a5988-5d111c1cba277269dc2dda2a5f4aaae384d4be3a.gz

--- Config.hs
+++ Config.hs
@@ -30,6 +30,7 @@ import Plugins.Date
 import Plugins.PipeReader
 import Plugins.CommandReader
 import Plugins.StdinReader
+import Plugins.XMonadLog
 
 #ifdef INOTIFY
 import Plugins.Mail
@@ -90,7 +91,7 @@ infixr :*:
 -- the 'Runnable.Runnable' Read instance. To install a plugin just add
 -- the plugin's type to the list of types (separated by ':*:') appearing in
 -- this function's type signature.
-runnableTypes :: Command :*: Monitors :*: Date :*: PipeReader :*: CommandReader :*: StdinReader :*:
+runnableTypes :: Command :*: Monitors :*: Date :*: PipeReader :*: CommandReader :*: StdinReader :*: XMonadLog :*:
 #ifdef INOTIFY
                  Mail :*:
 #endif
